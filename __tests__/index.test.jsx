a93eb43edb41c973922c557f2b9cdb8d031767f9
Josh Wallace <josh.wallace2@washpost.com>
2022-08-24 15:04:34 -0400

    add back failing test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -13,7 +13,7 @@ describe("Home", () => {
     expect(heading).toBeInTheDocument();
   });
 
-  // it("tests a failure", () => {
-  //   expect(false).toBeTruthy();
-  // });
+  it("tests a failure", () => {
+    expect(false).toBeTruthy();
+  });
 });
